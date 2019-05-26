dd917a03de5abc5f156eb288f26349533cacf164
duchunter <duc71098@gmail.com>
2019-05-26 22:46:11 +0700

    add login

--- pages/app.vue
+++ pages/app.vue
@@ -1,33 +1,36 @@
 <template>
   <main>
-      <div class="layout">
-          <!-- Start of Navigation -->
-          <Navigation />
-          <!-- End of Navigation -->
+    <div class="layout">
+      <!-- Start of Navigation -->
+      <Navigation />
+      <!-- End of Navigation -->
 
-          <!-- Start of Sidebar -->
-          <Sidebar />
-          <!-- End of Sidebar -->
+      <!-- Start of Sidebar -->
+      <Sidebar />
+      <!-- End of Sidebar -->
 
-          <!-- Start of Add Friends -->
-          <NewFriend />
-          <!-- End of Add Friends -->
+      <!-- Start of Add Friends -->
+      <NewFriend />
+      <!-- End of Add Friends -->
 
-          <!-- Start of Create Chat -->
-          <NewChat />
-          <!-- End of Create Chat -->
+      <!-- Start of Create Chat -->
+      <NewChat />
+      <!-- End of Create Chat -->
 
-          <div class="main">
-              <div class="tab-content" id="nav-tabContent">
-                  <!-- List of chat window goes here -->
-                  <!-- Or you can change only 1 chat window props -->
-                  <!-- Start of Babble -->
-                  <Babble />
-                  <!-- End of Babble -->
-              </div>
-          </div>
+      <div class="main">
+        <div
+          id="nav-tabContent"
+          class="tab-content"
+        >
+          <!-- List of chat window goes here -->
+          <!-- Or you can change only 1 chat window props -->
+          <!-- Start of Babble -->
+          <Babble />
+          <!-- End of Babble -->
+        </div>
       </div>
-      <!-- Layout -->
+    </div>
+    <!-- Layout -->
   </main>
 </template>
 
@@ -37,6 +40,8 @@ import Sidebar from '~/components/app/sidebar';
 import NewFriend from '~/components/app/modal/NewFriend';
 import NewChat from '~/components/app/modal/NewChat';
 import Babble from '~/components/app/chat/Babble';
+
+import { SERVER } from '~/config'
 
 import io from 'socket.io-client';
 
@@ -57,20 +62,19 @@ export default {
         { rel: 'stylesheet', href: '/css/swipe.min.css' },
       ],
       script: [
-        { src: '/js/jquery-3.3.1.slim.min.js', body: true },
-        { src: '/js/vendor/popper.min.js', body: true },
-        { src: '/js/swipe.min.js', body: true },
-        { src: '/js/bootstrap.min.js', body: true }
+        { src: '/js/jquery-3.3.1.slim.min.js', body: true, defer: true },
+        { src: '/js/vendor/popper.min.js', body: true, defer: true },
+        { src: '/js/swipe.min.js', body: true, defer: true },
+        { src: '/js/bootstrap.min.js', body: true, defer: true }
       ]
     }
   },
 
   mounted() {
-    this.$cookies.set('username', 'test', {
-      path: '/',
-      httpOnly: true,
-      secure: true
-    });
+    const username = this.$cookies.get('username');
+    if (!username) {
+      this.$router.push('/');
+    }
     // const socket = io('http://localhost:3001');
     // socket.on('connect', function(){});
     // socket.on('disconnect', function(){});
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,126 @@
 <template lang="html">
-
+  <div class="login">
+    <el-card>
+      <h2>Login</h2>
+      <el-form
+        ref="form"
+        class="login-form"
+        :model="model"
+        :rules="rules"
+        @submit.native.prevent="login"
+      >
+        <el-form-item prop="username">
+          <el-input
+            v-model="model.username"
+            placeholder="Username"
+            prefix-icon="fas fa-user"
+          />
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="model.password"
+            placeholder="Password"
+            type="password"
+            prefix-icon="fas fa-lock"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            :loading="loading"
+            class="login-button"
+            type="primary"
+            native-type="submit"
+            block
+          >
+            Login
+          </el-button>
+        </el-form-item>
+        <a
+          class="forgot-password"
+        >
+          Don't have an account?
+        </a>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import { SERVER } from '~/config'
 export default {
-  fetch ({ params, redirect }) {
-    redirect(301, '/app')
+  data() {
+    return {
+      model: {
+        username: "",
+        password: ""
+      },
+      loading: false,
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "Username is required",
+            trigger: "blur"
+          },
+          {
+            min: 4,
+            message: "Username length should be at least 5 characters",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "Password is required", trigger: "blur" },
+          {
+            min: 5,
+            message: "Password length should be at least 5 characters",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    let username = this.$cookies.get('username');
+    if (username) {
+      this.$router.push('/app');
+    }
+  },
+  head() {
+    return {
+      link: [
+        { rel: 'stylesheet', href: '/css/login.css' },
+        { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css', integrity: "sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf", crossorigin: "anonymous" },
+      ]
+    }
+  },
+  methods: {
+    async login() {
+      let valid = await this.$refs.form.validate();
+      if (!valid) {
+        return;
+      }
+      this.loading = true;
+      try {
+        let response = await this.$axios.post(`${SERVER}/signin`, this.model);
+        this.$cookies.set('username', response.data.username, {
+          path: "/",
+          maxAge: 60 * 60 * 24 * 365
+        });
+        this.$router.push('/app');
+      } catch (e) {
+        if (e.response && e.response.data) {
+          this.$message.error(e.response.data.message);
+        } else {
+          this.$message.error(e.message);
+        }
+      } finally {
+        this.loading = false;
+      }
+    }
   }
 }
 </script>
 
 <style lang="css" scoped>
+
 </style>
