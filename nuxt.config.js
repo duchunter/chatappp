4407e79e21664effd01129a9490b6e47bdf2b302
duchunter <duc71098@gmail.com>
2019-05-29 22:33:10 +0700

    hot fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,10 +17,10 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
-      { src: '/js/jquery-3.3.1.slim.min.js', body: true },
-      { src: '/js/vendor/popper.min.js', body: true },
-      { src: '/js/swipe.min.js', body: true },
-      { src: '/js/bootstrap.min.js', body: true }
+      // { src: '/js/jquery-3.3.1.slim.min.js', body: true },
+      // { src: '/js/vendor/popper.min.js', body: true },
+      // { src: '/js/swipe.min.js', body: true },
+      // { src: '/js/bootstrap.min.js', body: true }
     ]
   },
 
